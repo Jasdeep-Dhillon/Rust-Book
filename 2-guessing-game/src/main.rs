c20a556134aa8b31afaf30d2203a8d5e046e2df6
Jasdeep Singh Dhillon <59929844+Jasdeep-Dhillon@users.noreply.github.com>
2025-09-11 12:04:49 -0400

    3 Remove secret number in console, error handling when parsing string to u16, taking input in the same line

--- 2-guessing-game/src/main.rs
+++ 2-guessing-game/src/main.rs
@@ -1,24 +1,29 @@
 use std::cmp::Ordering;
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
     println!("Guess the number!");
 
     let secret_number: u16 = rand::random_range(1..=100);
-    println!("The secret number is {}", secret_number);
-
-    println!("Please input your guess");
 
     loop {
         let mut guess = String::new();
+        print!("Please input your guess: ");
+        io::stdout().flush().expect("Failed to flush stdout");
 
         io::stdin()
             .read_line(&mut guess)
             .expect("Failed to read line");
 
-        let guess: u16 = guess.trim().parse().expect("Please type a number");
+        let guess: u16 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(err) => {
+                println!("Enter a valid number, {:?}", err.to_string());
+                continue;
+            }
+        };
 
-        println!("You guessed: {guess}");
+        // println!("You guessed: {guess}");
         match guess.cmp(&secret_number) {
             Ordering::Less => println!("Too small!"),
             Ordering::Greater => println!("Too big!"),
