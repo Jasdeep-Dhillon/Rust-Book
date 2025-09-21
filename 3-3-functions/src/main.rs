434ddf8ec48c6c946b775b6981ab53b039cc467b
Jasdeep Singh Dhillon <59929844+Jasdeep-Dhillon@users.noreply.github.com>
2025-09-21 11:54:31 -0400

    3.3 Functions

--- 3-3-functions/src/main.rs
+++ 3-3-functions/src/main.rs
@@ -2,6 +2,14 @@ fn main() {
     println!("Hello, World");
     another_function();
     print_labeled_measurement(5, 'h');
+    
+    let y = {
+        let x = 3;
+        x + 1 // returns the value of x + 1
+    };
+    println!("The value of y is: {}", y);
+    println!("The value of five() is: {}", five());
+    println!("The value of plus_one(5) is: {}", plus_one(5));
 }
 
 fn print_labeled_measurement(value: i32, unit_label: char) {
@@ -10,4 +18,12 @@ fn print_labeled_measurement(value: i32, unit_label: char) {
 
 fn another_function() {
     println!("Printing from another funciton");
+}
+
+fn five() -> i32 /* return type */ {
+    5 // no semicolon returns the expression
+}
+
+fn plus_one(x: i32) -> i32 {
+    x + 1
 }
