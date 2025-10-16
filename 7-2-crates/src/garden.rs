7596b546a1669cab93762e7438d10fdbe4496dbc
Jasdeep Singh Dhillon <59929844+Jasdeep-Dhillon@users.noreply.github.com>
2025-10-15 21:53:41 -0400

    7.2 Binary and Library crates

---
+++ 7-2-crates/src/garden.rs
@@ -0,0 +1,3 @@
+// Garden Module
+// Using public module to access Asparagus struct inside vegetables
+pub mod vegetables;
---
+++ 7-2-crates/src/garden/vegetables.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+// Public struct to access it main
+pub struct Asparagus {
+    // Private variable to limit access
+    weight: u32,
+}
+
+impl Default for Asparagus {
+    fn default() -> Self {
+        Asparagus { weight: 32 }
+    }
+}
+
+impl Asparagus {
+    // Creating a function to access weight 
+    pub fn get_weight(&self) -> u32 {
+        self.weight
+    }
+}
